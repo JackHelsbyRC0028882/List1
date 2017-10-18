fcbd74091c5da713d6893b81b7f8b679e2b8ed84
RC0028882 <RC0028882@CRO-ICF07-22>
2017-10-18 15:55:12 +0100

    something

--- List1/Form1.cs
+++ List1/Form1.cs
@@ -18,18 +18,35 @@ namespace List1
             {
                 richTextBox1.AppendText(i + ",");
             }
+            richTextBox1.AppendText("\n================\n");
         }
         public Form1()
         {
             InitializeComponent();
-            List<int> ints = new List<int>();
+            List<int> nums = new List<int>();
             nums.Add(3);
             nums.Add(5);
             nums.Add(3);
             printNums(nums);
-            richTextBox1.AppendText("\n================\n");
-            ints.Remove(3);
+           
+            nums.Remove(3);
             printNums(nums);
+            List<int> ints = new List<int>();
+            ints.Add(1);
+            ints.Add(2);
+            ints.Add(3);
+            ints.Add(4);
+            ints.Add(5);
+            ints.Add(6);
+            ints.Add(7);
+            ints.Add(8);
+            ints.Add(9);
+            ints.Add(10);
+            printNums(ints);
+            ints.Remove(1);
+            printNums(ints);
+            printNums(ints);
+
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
